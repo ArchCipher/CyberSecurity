882a90421cf8c0a92a13bca79aacf19e8e8c2ba1
ArchCipher <kruthika.arch@gmail.com>
2025-08-27 17:07:05 +0900

    v2

--- C-lang/cs50-solutions/pset1/credit.c
+++ C-lang/cs50-solutions/pset1/credit.c
@@ -8,6 +8,7 @@ char *get_input() {
     return card_num;
 }
 
+// Luhn checksum algorithm
 int checksum(char *card_num, int *length) {
     int check=0;
     *length = strlen(card_num);
@@ -25,22 +26,37 @@ int checksum(char *card_num, int *length) {
     return check%10;
 }
 
+int get_prefix (char *card_num, int digits){
+    int prefix = 0;
+    for (int i=0; i < digits && card_num[i]; i++) {
+    prefix = prefix * 10 + (card_num[i]-'0');
+    }
+    return prefix;
+}
+
+void print_card_type (char *card_num, int length, int check){
+    int first = get_prefix(card_num, 1);
+    int first_two = get_prefix(card_num, 2);
+    int first_four = get_prefix(card_num, 4);
+    if (check == 0) {
+        if (length == 15 && (first_two == 34 || first_two == 37)) {
+            printf("American Express");
+        }
+        else if (length == 16 && ((first_two >= 51 && first_two <= 55) || (first_four >= 2221 && first_four <= 2720))) {
+            printf("MasterCard");   // include new range of mastercard 2221-2720
+        }
+        else if ((length == 13 || length == 16) && first == 4) {
+            printf("VISA");
+
+        }
+        else printf("INVALID");
+    }
+}
+
 int main () {
 char *card_num = get_input();
 int length = 0;
 int check = checksum (card_num, &length);
-
-int first_four= (card_num[0]-'0')*1000 + (card_num[1]-'0')*100 + (card_num[2]-'0')*10 + (card_num[3]-'0');//new range of mastercard 2221-2720
-
-if (check == 0 && length ==15 && card_num[0] == '3' && (card_num[1]=='4' || card_num[1]=='7')) {
-printf ("American Express");
-}
-else if (check == 0 && length == 16 && ((card_num[0] == '5' && card_num[1] >= '1' && card_num[1] <= '5') || (first_four >=2221 && first_four <=2720 ))) {
-printf("MasterCard");
-}
-else if (check == 0 && (length == 13 || length == 16) && card_num[0] =='4') {
-printf("VISA");
-}
-else printf("INVALID");
+print_card_type(card_num, length, check);
 return 0;
 }
